6f446f73ca8c6218df61e2486b7a12bbedda6c95
Mortaza Amiri <imortazaamiri@gmail.com>
2026-09-14 19:42:27 +0000

    lab 1 compelet

---
+++ lab01/hello.py
@@ -0,0 +1,8 @@
+
+name = input("Waht is yor name?")
+course = input("what course are you studying?")
+interest = input("What area of cybersecurity interests you?")
+
+print("Hello, " +name+"!")
+print("you are studying, " +course + "!")
+print("your interests, " + interests + "!")
---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name =input  ("what is your name?")
+student_ID =input  ("what is your student number?")
+year  =input ("what year are you in?")
+
+print (" hello, ",name)
+print ("your student number is", student_ID)
+print ("year", year)
